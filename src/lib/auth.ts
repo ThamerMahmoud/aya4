d198d345d1fd16a54a53c0b5ea03047a9c68df16
Thamer Mahmoud <thamer84@gmail.com>
2026-06-07 10:59:17 +0300

    message

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,9 +1,27 @@
-import { betterAuth } from 'better-auth'
-import { tanstackStartCookies } from 'better-auth/tanstack-start'
+import { betterAuth } from "better-auth";
+import { tanstackStartCookies } from "better-auth/tanstack-start";
+import { Pool } from "pg";
 
 export const auth = betterAuth({
+  database: new Pool({
+    // connectionString: 'postgres://postgres:123456@localhost:5440/postgres',
+    connectionString:
+      "postgres://b6c5bcc1ae54f74f8284b58f892621a044c90c89f33033d97b72ca6ae2a715ba:sk_PjyizDt0vqNBuVkKMki-L@db.prisma.io:5432/postgres?sslmode=require",
+  }),
+  user: {
+    additionalFields: {
+      role: {
+        type: ["Student", "Teacher"],
+        required: true,
+        defaultValue: "Student",
+        input: true, // don't allow user to set role
+      },
+    },
+  },
+
   emailAndPassword: {
     enabled: true,
   },
+
   plugins: [tanstackStartCookies()],
-})
+});
